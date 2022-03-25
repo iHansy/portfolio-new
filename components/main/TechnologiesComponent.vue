1ae3eb787d971677d5b26a9e3e4cba0230567544
Hans Accola <hansjaccola@gmail.com>
2022-03-24 21:49:02 -0500

    tech images complete in desktop, need to add divider and make it responsive

--- components/main/TechnologiesComponent.vue
+++ components/main/TechnologiesComponent.vue
@@ -16,67 +16,107 @@
           class="d-flex align-center justify-space-around ma-0"
           style="height: 50%"
         >
-          <img
+          <v-div
             v-for="(image, i) in languageTopImages"
             :key="i"
-            :height="image.height"
-            :width="image.width"
-            class="ma-2"
-            :src="require(`~/static/technologies/${image.src}`)"
-            alt="Javascript Icon"
-          />
+            style="width: 14%"
+            class="pt-4"
+          >
+            <div class="d-flex justify-center">
+              <img
+                height="65px"
+                width="65px"
+                class="mx-auto"
+                :src="require(`~/static/technologies/${image.src}`)"
+                alt="Javascript Icon"
+              />
+            </div>
+            <p class="text-center pt-2" style="font-size: 18px">
+              {{ image.name }}
+            </p>
+          </v-div>
         </v-row>
         <v-row
           class="d-flex align-center justify-space-around ma-0"
           style="height: 50%"
         >
-          <img
+          <v-div
             v-for="(image, i) in languageBottomImages"
             :key="i"
-            :height="image.height"
-            :width="image.width"
-            class="ma-2"
-            :src="require(`~/static/technologies/${image.src}`)"
-            alt="Javascript Icon"
-          />
+            style="width: 14%"
+            class="pt-2"
+          >
+            <div class="d-flex justify-center">
+              <img
+                height="65px"
+                width="65px"
+                class="mx-auto"
+                :src="require(`~/static/technologies/${image.src}`)"
+                alt="Javascript Icon"
+              />
+            </div>
+            <p class="text-center pt-2" style="font-size: 18px">
+              {{ image.name }}
+            </p>
+          </v-div>
         </v-row>
       </v-col>
     </v-row>
     <v-row class="mt-15 main-row">
       <v-col
         cols="2"
-        class="background-cyan main-col-left d-flex align-center justify-center"
+        class="background-cyan main-col-left d-flex align-center justify-center py-0"
       >
         <h2 class="text-center">Tools</h2>
       </v-col>
-      <v-col cols="10" class="background-yellow main-col-right">
+      <v-col cols="10" class="background-yellow main-col-right py-0">
         <v-row
           class="d-flex align-center justify-space-around ma-0"
           style="height: 50%"
         >
-          <img
+          <v-div
             v-for="(image, i) in toolTopImages"
             :key="i"
-            :height="image.height"
-            :width="image.width"
-            class="ma-2"
-            :src="require(`~/static/technologies/${image.src}`)"
-            alt="Javascript Icon"
-          />
+            style="width: 14%"
+            class="pt-4"
+          >
+            <div class="d-flex justify-center">
+              <img
+                height="65px"
+                width="65px"
+                class="mx-auto"
+                :src="require(`~/static/technologies/${image.src}`)"
+                alt="Javascript Icon"
+              />
+            </div>
+            <p class="text-center pt-2">
+              {{ image.name }}
+            </p>
+          </v-div>
         </v-row>
         <v-row
           class="d-flex align-center justify-space-around ma-0"
           style="height: 50%"
         >
-          <img
+          <v-div
             v-for="(image, i) in toolBottomImages"
             :key="i"
-            :height="image.height"
-            :width="image.width"
-            class="ma-2"
-            :src="require(`~/static/technologies/${image.src}`)"
-            alt="Javascript Icon"
-          />
+            style="width: 14%"
+            class="pt-2"
+          >
+            <div class="d-flex justify-center">
+              <img
+                height="65px"
+                width="65px"
+                class="mx-auto"
+                :src="require(`~/static/technologies/${image.src}`)"
+                alt="Javascript Icon"
+              />
+            </div>
+            <p class="text-center pt-2" style="font-size: 18px">
+              {{ image.name }}
+            </p>
+          </v-div>
         </v-row>
       </v-col>
     </v-row>
@@ -97,60 +137,50 @@ export default {
       titleId: 'technologies-title',
       timer: false,
       languageTopImages: [
-        { name: 'Html', src: 'html.png', height: '75px', width: '75px' },
-        { name: 'CSS', src: 'css.png', height: '75px', width: '75px' },
-        { name: 'SCSS', src: 'scss.png', height: '75px', width: '75px' },
-        { name: 'Vuetify', src: 'vuetify.png', height: '75px', width: '75px' },
+        { name: 'Html', src: 'html.png' },
+        { name: 'CSS', src: 'css.png' },
+        { name: 'SCSS', src: 'scss.png' },
+        { name: 'Vuetify', src: 'vuetify.png' },
         {
           name: 'Material UI',
-          src: 'material-ui.png',
-          height: '75px',
-          width: '75px'
+          src: 'material-ui.png'
         },
         {
           name: 'Javascript',
-          src: 'javascript.png',
-          height: '75px',
-          width: '75px'
+          src: 'javascript.png'
         },
-        { name: 'Nuxt.js', src: 'nuxtjs.png', height: '75px', width: '75px' }
+        { name: 'Nuxt.js', src: 'nuxtjs.png' }
       ],
       languageBottomImages: [
-        { name: 'Vue.js', src: 'vuejs.png', height: '75px', width: '75px' },
-        { name: 'React.js', src: 'reactjs.svg', height: '75px', width: '75px' },
-        { name: 'Nest.js', src: 'nestjs.svg', height: '75px', width: '75px' },
-        { name: 'Node.js', src: 'nodejs.png', height: '75px', width: '90px' },
+        { name: 'Vue.js', src: 'vuejs.png' },
+        { name: 'React.js', src: 'reactjs.svg' },
+        { name: 'Nest.js', src: 'nestjs.svg' },
+        { name: 'Node.js', src: 'nodejs.png' },
         {
           name: 'Postgresql',
-          src: 'postgresql.png',
-          height: '75px',
-          width: '75px'
+          src: 'postgresql.png'
         },
-        { name: 'Typeorm', src: 'typeorm.png', height: '75px', width: '75px' }
+        { name: 'Typeorm', src: 'typeorm.png' }
       ],
       toolTopImages: [
         {
           name: 'Phpstorm',
-          src: 'phpstorm.png',
-          height: '75px',
-          width: '75px'
+          src: 'phpstorm.png'
         },
-        { name: 'VS code', src: 'vscode.png', height: '75px', width: '75px' },
-        { name: 'Git', src: 'git.png', height: '75px', width: '75px' },
-        { name: 'Npm', src: 'npm.png', height: '75px', width: '75px' },
-        { name: 'Docker', src: 'docker.png', height: '75px', width: '75px' }
+        { name: 'VS code', src: 'vscode.png' },
+        { name: 'Git', src: 'git.png' },
+        { name: 'Npm', src: 'npm.png' },
+        { name: 'Docker', src: 'docker.png' }
       ],
       toolBottomImages: [
         {
           name: 'Bitbucket',
-          src: 'bitbucket.png',
-          height: '75px',
-          width: '75px'
+          src: 'bitbucket.png'
         },
-        { name: 'Github', src: 'github.png', height: '75px', width: '75px' },
-        { name: 'Heroku', src: 'heroku.png', height: '75px', width: '75px' },
-        { name: 'Jira', src: 'jira.png', height: '75px', width: '75px' },
-        { name: 'Monday', src: 'monday.png', height: '75px', width: '75px' }
+        { name: 'Github', src: 'github.png' },
+        { name: 'Heroku', src: 'heroku.png' },
+        { name: 'Jira', src: 'jira.png' },
+        { name: 'Monday', src: 'monday.png' }
       ]
     };
   }
